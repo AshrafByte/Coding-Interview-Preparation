a4d3006fdf2e5a015a33b2046921331925effc1d
Ahmed Ashraf <160413156+AshrafByte@users.noreply.github.com>
2024-10-02 16:50:12 +0300

    Updated 3-  Climbing Stairs.cpp

--- Week 2/Ahmed Ashraf/3-  Climbing Stairs.cpp
+++ Week 2/Ahmed Ashraf/3-  Climbing Stairs.cpp
@@ -3,11 +3,8 @@ public:
     unordered_map <int,int> ways ;
     int climbStairs(int n) 
     {
-        if (n < 0)
-            return 0 ;
-
-        if (n == 0)
-            return 1;
+        if (n <= 0)
+            return (n == 0) ;
         
         if (ways.count(n) == 0)
             ways[n] = climbStairs(n-1) + climbStairs(n-2);
